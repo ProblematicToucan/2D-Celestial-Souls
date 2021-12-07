e48d570d880beb9ef7cec01e54ae9e0bc82eab38
ProblematicToucan <gamal.aziz1000@gmail.com>
2021-12-07 21:57:54 +0700

    Collider & Layer setup
    Simpel setup collider object dan karakter.

--- 2D Celestial Souls/Assets/MyAssets/Script/PlayerLocomotion.cs
+++ 2D Celestial Souls/Assets/MyAssets/Script/PlayerLocomotion.cs
@@ -8,6 +8,7 @@ namespace GD.Script
     public class PlayerLocomotion : MonoBehaviour
     {
         private BoxCollider2D _boxCollider;
+        private RaycastHit2D _hit;
 
         private PlayerManager _playerManager;
 
@@ -26,9 +27,28 @@ namespace GD.Script
                 transform.localScale = Vector3.one;
             else if (moveDelta.x < 0)
                 transform.localScale = new Vector3(-1, 1, 1);
+
+            #region MovementLogic
+
+            // Make sure we can move in this y direction, by casting there first, if the box return null we are free to move
+            _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(0, moveDelta.y),
+                Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+            if (_hit.collider == null)
+            {
+                // Character Movement y direction
+                transform.Translate(0, moveDelta.y * Time.deltaTime, 0);    
+            }
             
-            // Character Movement
-            transform.Translate(moveDelta * Time.deltaTime);
+            _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(moveDelta.x, 0),
+                Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+            if (_hit.collider == null)
+            {
+                // Character Movement x direction
+                transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);    
+            }
+
+            #endregion
+            
         }
     }   
 }
